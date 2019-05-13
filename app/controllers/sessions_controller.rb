4a0ef63eb8f1673a6c128b8157401d0872ef382f
Ibidapo Rasheed <dapsined@gmail.com>
2019-05-13 15:30:53 +0100

    implement user login and session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # debugger
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back #{user.username}!"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Incorrect login credentials"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "We look forward to seeing you again :("
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   put 'upload/:id', to: 'users#upload'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
